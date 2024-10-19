1b8cd39f31086c0474fd369c30803fa143f5db71
Shashank Verma <shashank6341@gmail.com>
2024-10-19 14:08:07 -0400

    Exercises 1.1
    Completed Exercise 1.1

---
+++ main code/src/App.jsx
@@ -0,0 +1,24 @@
+import Footer from "./Footer";
+
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const friends = ["Peter", "Maya"];
+
+  return (
+    <div>
+      <p>{friends}</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ main code/src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by{" "}
+      <a href="https://github.com/shashank6341">Shashank</a>
+    </div>
+  );
+};
+
+export default Footer;
