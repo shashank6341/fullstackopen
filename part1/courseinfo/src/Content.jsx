95b8297f48d691f67139bad9b2f1c809feb13507
Shashank Verma <shashank6341@gmail.com>
2024-10-19 20:56:46 -0400

    Ex 1.4
    Completed Ex 1.4

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,11 +1,12 @@
 import Part from "./Part";
 
 const Content = (props) => {
+  // console.log(props);
   return (
     <>
-      <Part part={props.part1.name} exercises={props.part1.exercises} />
-      <Part part={props.part2.name} exercises={props.part2.exercises} />
-      <Part part={props.part3.name} exercises={props.part3.exercises} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   );
 };
--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,8 +1,11 @@
 const Total = (props) => {
+  console.log(props);
   return (
     <p>
       Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
     </p>
   );
 };
